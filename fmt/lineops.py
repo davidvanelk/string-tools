971a0d271ed3236a30e0a4398dc3eb622b505785
davidvanelk <d.elk@probat.com>
2018-11-09 11:10:42 +0100

    parameter call

--- fmt/lineops.py
+++ fmt/lineops.py
@@ -1,9 +1,19 @@
 """
-Format the lines (s) of an array, where it should begin with b and end with e
+Format the lines (s) of a string, where it should begin with b and end with e
 """
 def appendLines(s, b, e):
-    i = 0
-    for l in s:
-        s[i] = b + l + e
-        i += 1
-    return s
+    retval = ""
+    arr = s.splitlines()
+    for l in arr:
+        retval += b + l + e + "\n"
+    return retval
+
+def tabLines(s, tabs):
+    retval = ""
+    arr = s.splitlines()
+    for l in arr:
+        istr = ""
+        for i in range(0, tabs):
+            istr += "\t"
+        retval += istr + l + "\n"
+    return retval
--- stool.py
+++ stool.py
@@ -3,12 +3,56 @@ from convert import binary as binary
 from fmt import blockformat, lineops
 import sys
 
-for param in sys.argv:
-    print(param)
 
-x = binary.stox("Hello World!")
-x = blockformat.blockLines(x, 8, 4)
-x = lineops.appendLines(x, "\t$\"", "\"")
+x = str("")
+filename = str("")
+beginline = None
+endline = None
+blocks = None
+blocklength = None
+hexConversion = False
+tabs = 0
 
-for l in x:
-    print(l)
+for param in sys.argv[1:]:
+    if not param.startswith('--'):
+        if len(x) > 0:
+            x += " "
+        x += param
+    else:
+        param = param[2:]
+        arr = param.split('=')
+        if arr[0] == "file":
+            filename = arr[1]
+        elif arr[0] == "beginline":
+            beginline = ''.join(arr[1:])
+        elif arr[0] == "endline":
+            endline = ''.join(arr[1:])
+        elif arr[0] == "blocks":
+            blockconfig = arr[1].split('x')
+            blocks = int(blockconfig[0])
+            blocklength = int(blockconfig[1])
+        elif arr[0] == "hex":
+            hexConversion = True
+        elif arr[0] == "tabs":
+            tabs = int(arr[1])
+
+print(x)
+if len(filename) > 0:
+    print(filename)
+    f = open(filename, "r")
+    x = f.read()
+
+if hexConversion:
+    x = binary.stox("Hello World!")
+
+if blocks != None and blocklength != None:
+    x = blockformat.blockLines(x, 8, 4)
+
+if beginline != None:
+    x = lineops.appendLines(x, beginline, "")
+if endline != None:
+    x = lineops.appendLines(x, "", endline)
+if tabs > 0:
+    x = lineops.tabLines(x, tabs)
+
+print(x)
